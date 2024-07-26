cb8bc02502efe809f24378b76aac89a5804ce94c
JonnyDeates <jonnydeates@gmail.com>
2024-07-26 15:49:15 -0500

    fix: Updates the className to not join with the wrapper, removes the ability for user to scroll when modal is open.

--- lib/components/Modals/GenericModalBase/index.tsx
+++ lib/components/Modals/GenericModalBase/index.tsx
@@ -1,33 +1,46 @@
-import {HTMLAttributes, ReactNode} from "react"
+import {HTMLAttributes, ReactNode, useEffect} from "react"
 import {CloseButton} from "../../Buttons/CloseButton"
 import styles from './styles.module.css'
 
 export type GenericModalBaseProps = {
-  handleClose: () => void, isOpen: boolean, children: ReactNode,
-  modalAttributes?: HTMLAttributes<HTMLDivElement>,
-  modalWrapperAttributes?: HTMLAttributes<HTMLDivElement>,
+    handleClose: () => void, isOpen: boolean, children: ReactNode,
+    modalAttributes?: HTMLAttributes<HTMLDivElement>,
+    modalWrapperAttributes?: HTMLAttributes<HTMLDivElement>,
 }
 
 export function GenericModalBase({
-                                   handleClose,
-                                   isOpen,
-                                   children,
-                                   modalAttributes = {},
-                                   modalWrapperAttributes = {}
+                                     handleClose,
+                                     isOpen,
+                                     children,
+                                     modalAttributes = {},
+                                     modalWrapperAttributes = {}
                                  }: GenericModalBaseProps) {
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.maxHeight = '100vh';
+            document.body.style.overflowY = 'hidden';
+        } else {
+            document.body.style.maxHeight = "unset";
+            document.body.style.overflowY = 'unset';
+        }
+        return () => {
+            document.body.style.maxHeight = "unset";
+            document.body.style.overflowY = 'unset';
+        }
+    }, [isOpen]);
 
-  if (!isOpen) {
-    return <></>
-  }
+    if (!isOpen) {
+        return <></>
+    }
 
-  const {className = ''} = modalAttributes
-  const {className: modalWrapperClassName = ''} = modalWrapperAttributes
+    const {className = ''} = modalAttributes
+    const {className: modalWrapperClassName = ''} = modalWrapperAttributes
 
-  return <>
-    <div role='presentation' className={`${styles.ModalWrapper }${modalWrapperClassName}`} onClick={handleClose}/>
-    <div {...modalAttributes} className={`${styles.ModalCard} ${className}`}>
-      <CloseButton onClick={handleClose}/>
-      {children}
-    </div>
-  </>
+    return <>
+        <div role='presentation' className={`${styles.ModalWrapper} ${modalWrapperClassName}`} onClick={handleClose}/>
+        <div {...modalAttributes} className={`${styles.ModalCard} ${className}`}>
+            <CloseButton onClick={handleClose}/>
+            {children}
+        </div>
+    </>
 }
